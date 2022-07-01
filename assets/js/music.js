56a1556e5dbc671374a59cb27bd0d6fb34368a9d
kylianAlger <kylian.alger.leonard@gmail.com>
2022-07-01 01:00:07 +0100

    :sparkles: Add new features

---
+++ assets/js/music.js
@@ -0,0 +1,30 @@
+const buttonSound = document.querySelector('.sound')
+const audio = new Audio('./assets/audio/ireliaMusic.mp3')
+const on = buttonSound.querySelector('.musicOn')
+const off = buttonSound.querySelector('.musicOff')
+
+audio.volume = 0.08
+audio.pause()
+
+let count = 0
+
+buttonSound.addEventListener('click', (event) =>{
+
+    if(count == 0){
+
+        count = 1
+        audio.play()
+        on.style.display = "block"
+        off.style.display = "none"
+
+    } else if(count == 1){
+
+        count = 0
+        audio.pause()
+        audio.currentTime = 0
+        on.style.display = "none"
+        off.style.display = "block"
+
+    }
+
+})
